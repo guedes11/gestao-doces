03c45aa35cab3529f5806707392289d4542d2bd1
Kevem Guedes <99044074+guedes11@users.noreply.github.com>
2026-03-26 14:19:06 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'doces-app-v2';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Instalação do PWA
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Ativação e limpeza de caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Interceptar requisições (Modo Offline)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
